ac18d40a3c61114d7cf0c695f0a3a3975eccef51
eduard <eduardsteveen@gmail.com>
2023-05-02 08:43:05 -0500

    entrega actividad listas

---
+++ 02_sintaxis/index.js
@@ -0,0 +1,9 @@
+fecha = new Date(2003, 5, 3);
+libro = {
+    titulo: "La fierecilla domada",
+    autor: "William Shakespeare",
+    fecha: new Date(2000, 5, 5),
+    url: "https://infolibros-org.ucompensar.basesdedatosezproxy.com/pdfview/20573-la-fierecilla-domada-william-shakespeare/"
+}
+
+const lista=["Eduard",19,false,fecha,libro];
