ff09be167728c89e87306cc67679e95b1ff779b9
eduard <eduardsteveen@gmail.com>
2023-05-03 21:22:14 -0500

    entrega actividad strings

---
+++ 04_strings/strings.js
@@ -0,0 +1,10 @@
+let nombre = "Eduard";
+let apellido = "Suarez";
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let cont = estudiante.length
+let primerLetraNom = nombre[0];
+let ultimaLetraAp = estudiante[estudiante.length-1];
+let sinEspacios = estudiante.trim();
+let estaNombre = estudiante.includes(nombre);
