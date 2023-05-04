08cc6d1a15312102dc18dee138588314c9a57b98
eduard <eduardsteveen@gmail.com>
2023-05-04 08:58:11 -0500

    entega actividad numeros

--- 05_numbers/numbers.js
+++ 05_numbers/numbers.js
@@ -2,4 +2,5 @@ let alturaCM = 177;
 let alturaM = 1.77;
 let peso = 64.5;
 let alturaM_re= Math.round(alturaM);
-let peso_re = Math.floor(peso);
+let peso_re = Math.floor(peso)
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
