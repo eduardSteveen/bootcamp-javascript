07214ae82bb5bb7661891712bcef0f29f4479e20
eduard <eduardsteveen@gmail.com>
2023-05-05 16:25:49 -0500

    entrega actividad fechas

---
+++ 07_otros_obj/fecha.js
@@ -0,0 +1,6 @@
+const fecha = new Date();
+const nacimiento = new Date(2003,05,3);
+const comparacion = fecha>nacimiento;
+const dia = nacimiento.getDate();
+const mes = nacimiento.getMonth();
+const año = nacimiento.getFullYear();
